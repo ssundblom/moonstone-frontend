662cd333c68ba4cc6ce8f41f5384ecfaa2c2dec9
Sara Sundblom <sundblomsara@gmail.com>
2021-01-22 09:40:23 +0100

    downloaded faviconpackage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const HeaderContainer = styled.div`
+  display: flex;
+
+`
+const Header = styled.div`
+  font-size: 20px;
+  color: #6C6C5D;
+
+`
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <HeaderContainer>
+      <Header>
+        WELCOME TO MOONSTONE!
+      </Header>
+    </HeaderContainer>
   )
 }
