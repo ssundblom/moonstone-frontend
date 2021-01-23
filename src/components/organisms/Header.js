7cf53bbbae266e7753dd30923b21ab6dd14e97c7
Sara Sundblom <sundblomsara@gmail.com>
2021-01-23 11:04:40 +0100

    logo in place

--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { styled } from 'styled-components'
 
 import { Hamburger } from '../atoms/Hamburger'
 import { Cart } from '../atoms/Cart'
+import { ReactComponent as Logo} from '../../assets/images/logo.svg'
 
 export const Header = () => {
   return (
     <>
       <Hamburger />
+      <Logo />
       <Cart />
     </>
   )
