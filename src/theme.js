53eca582341abc704ffaa9946d3614198c0b0aab
Sara Sundblom <sundblomsara@gmail.com>
2021-01-23 11:39:10 +0100

    made colorschema

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    primary: '#a5a58d',
+    primaryAlt: '#b7b7a4',
+    secondary: '#eddcd2',
+    secondaryAlt: '#fff1e6',
+    text: '#848480',
+    textAlt: '#f0efeb',
+    link: '#cb997e'
+  }
+}
