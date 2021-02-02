2a3937813063abcc5c93ae849ff90139718c793a
Sara Sundblom <sundblomsara@gmail.com>
2021-02-02 10:20:19 +0100

    added hero image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,28 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import { Link } from 'react-router-dom'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Image from 'react-bootstrap/Image'
 
+const StyledJumbo = styled(Jumbotron)`
+  padding: 0;
+  margin: 0;
+  background-image: url(${process.env.PUBLIC_URL}/sage.jpg);
+  background-size: cover;
+  background-position: center center;
+  height: 90vh;
+`
 
 export const Home = () => {
   return (
-    <Container className="text-center justify-content-center align-items-center">
-      <Row style={{height: '50vh'}} className="align-items-center" xs="1" sm="1" md="1" lg="1" xl="1">
-        <Col>
-          <p> Find the perfect selection of soul-nurturing goodies, for you</p>
-        </Col>
-        <Col>
-          <Link to="/create-mix">
-            <Button>Create your mix</Button>
-          </Link>
-        </Col>
-        <Col>
-          <Link to="/shop">Or browse all products</Link>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <StyledJumbo fluid>
+
+        <Container className="text-center">
+          <Row style={{ height: '35vh' }} className="align-items-center align-content-around" xs="1">
+            <Col>
+              <p> Find the perfect selection of soul-nurturing goodies, for you</p>
+            </Col>
+            <Col>
+              <Link to="/create-mix">
+                <Button>Create your mix</Button>
+              </Link>
+            </Col>
+            <Col>
+              <Link to="/shop">Or browse all products</Link>
+            </Col>
+          </Row>
+        </Container>
+      </StyledJumbo>
+    </>
   )
 }
