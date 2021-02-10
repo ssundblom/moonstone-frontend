944e49508717c3f2fe1e4b37c88123ca8a41a42c
Sara Sundblom <sundblomsara@gmail.com>
2021-02-10 19:05:10 +0100

    made small change in checkout

--- src/components/pages/CheckOut.js
+++ src/components/pages/CheckOut.js
@@ -9,7 +9,7 @@ const StyledContainer = styled(Container)`
 export const CheckOut = () => {
   return (
     <StyledContainer>
-      <p>Hey there! I'm so happy that you found something in the shop! I really hope in the near future that I'll be able to send your soul goodies to you</p>
+      <p>Hey there! I'm so happy that you found something in the shop! I really hope in the near future that I'll be able to send your soul goodies to you. Send me an email and I'll let you know as soon as we are up n running</p>
     </StyledContainer>
   )
 }
